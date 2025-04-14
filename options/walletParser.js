550e7c08be1b13726c44ba3dac639eee376a21cb
Hropotova <68398910+Hropotova@users.noreply.github.com>
2025-04-14 22:53:15 +0300

    FIX | Fixed pnl calculate and add comments for code

--- options/walletParser.js
+++ options/walletParser.js
@@ -17,151 +17,164 @@ const walletParser = async (addresses, bot, chatId) => {
             // Get all transactions for the wallet address.
             const transactions = await getAllTransactions(address);
 
-            // Get all swap related transactions (buy, sell).
-            const swaps = await getWalletTokenSwaps(address);
+            if (transactions.length < process.env.TRANSACTIONS_COUNT) {
 
-            // Get token balances for a specific wallet address.
-            const balances = await getWalletTokenBalances(address);
+                // Get all swap related transactions (buy, sell).
+                const swaps = await getWalletTokenSwaps(address);
 
-            // Get the active chains for a wallet address.
-            const chains = await getActiveWalletChains(address);
+                // Get token balances for a specific wallet address.
+                const balances = await getWalletTokenBalances(address);
 
-            // Get lost swaps and transfers.
-            const {lostSwaps, transfers} = await checkTransactionHistory(address, swaps);
+                // Get the active chains for a wallet address.
+                const chains = await getActiveWalletChains(address);
 
-            const tokenData = {};
+                // Get lost swaps and transfers.
+                const {lostSwaps, transfers} = await checkTransactionHistory(address, swaps);
 
-            // Compare the swaps with the lost swaps.
-            const allSwaps = [...swaps, ...lostSwaps];
+                const tokenData = {};
 
-            for (const swap of allSwaps) {
-                const {bought, sold, transactionType} = swap;
-                if (!bought || !sold) continue;
+                // Compare the swaps with the lost swaps.
+                const allSwaps = [...swaps, ...lostSwaps];
 
-                const boughtSymbol = bought.symbol;
-                const soldSymbol = sold.symbol;
-                const boughtAddress = bought.address;
-                const soldAddress = sold.address;
+                for (const swap of allSwaps) {
+                    const {bought, sold, transactionType} = swap;
+                    if (!bought || !sold) continue;
 
-                // Handle BUY transactions.
-                if (transactionType === 'buy') {
-                    const token = boughtAddress;
-                    if (!tokenData[token]) {
-                        tokenData[token] = {
-                            boughtAmount: 0,
-                            soldAmount: 0,
-                            spent: 0,
-                            received: 0,
-                            contractAddress: boughtAddress,
-                            symbol: boughtSymbol,
-                            balance: 0,
-                        };
+                    const boughtSymbol = bought.symbol;
+                    const soldSymbol = sold.symbol;
+                    const boughtAddress = bought.address;
+                    const soldAddress = sold.address;
+
+                    // Handle BUY transactions.
+                    if (transactionType === 'buy') {
+                        const token = boughtAddress;
+                        if (!tokenData[token]) {
+                            tokenData[token] = {
+                                boughtAmount: 0,
+                                soldAmount: 0,
+                                spent: 0,
+                                received: 0,
+                                contractAddress: boughtAddress,
+                                symbol: boughtSymbol,
+                                balance: 0,
+                            };
+                        }
+                        tokenData[token].boughtAmount += parseFloat(bought.amount);
+                        tokenData[token].spent += Math.abs(parseFloat(sold.amount));
                     }
-                    tokenData[token].boughtAmount += parseFloat(bought.amount);
-                    tokenData[token].spent += Math.abs(parseFloat(sold.amount));
+
+                    // Handle SELL transactions.
+                    if (transactionType === 'sell') {
+                        const token = soldAddress;
+                        if (!tokenData[token]) {
+                            tokenData[token] = {
+                                boughtAmount: 0,
+                                soldAmount: 0,
+                                spent: 0,
+                                received: 0,
+                                contractAddress: soldAddress,
+                                symbol: soldSymbol,
+                                balance: 0,
+                            };
+                        }
+                        tokenData[token].soldAmount += Math.abs(parseFloat(sold.amount));
+                        tokenData[token].received += parseFloat(bought.amount);
+                    }
                 }
 
-                // Handle SELL transactions.
-                if (transactionType === 'sell') {
-                    const token = soldAddress;
-                    if (!tokenData[token]) {
-                        tokenData[token] = {
-                            boughtAmount: 0,
-                            soldAmount: 0,
-                            spent: 0,
-                            received: 0,
-                            contractAddress: soldAddress,
-                            symbol: soldSymbol,
-                            balance: 0,
-                        };
+
+                // Convert USD balances to WBNB equivalents.
+                for (const token of balances) {
+                    const contractAddress = token.token_address;
+                    if (tokenData[contractAddress]) {
+                        const usdValue = token.usd_value || 0;
+                        tokenData[contractAddress].balance = usdValue / bnbPrice.usdPrice.toFixed(4);
                     }
-                    tokenData[token].soldAmount += Math.abs(parseFloat(sold.amount));
-                    tokenData[token].received += parseFloat(bought.amount);
                 }
-            }
 
-            console.log('tokenData', tokenData)
-
-            // Convert USD balances to WBNB equivalents.
-            for (const token of balances) {
-                console.log('token', token)
-                const contractAddress = token.token_address;
-                if (tokenData[contractAddress]) {
-                    const usdValue = token.usd_value || 0;
-                    tokenData[contractAddress].balance = usdValue / bnbPrice.usdPrice.toFixed(4);
-                }
-            }
-
-            // Add calculated data to JSON.
-            const addressData = {
-                target_chain: 'bsc', // Add target chain from .env file to the JSON.
-                active_chains: chains, // Add active chains to the JSON
-                address_info: {
-                    total_transactions: transactions.length,
-                    total_tokens_traded: Object.entries(tokenData).length,
-                },
-                traded_tokens: {},
-            };
-
-            for (const [contract, stats] of Object.entries(tokenData)) {
-                // Calculate PnL for the token: balance + (received - spent).
-                const realizedPnl = stats.balance + (stats.received - stats.spent);
-
-                // Count inflow and outflow transactions.
-                let inflow_count = 0;
-                let outflow_count = 0;
-
-                const tokenTransfers = transfers.filter(i => i.contract === contract);
-
-                tokenTransfers.forEach(transfer => {
-                    if (transfer.from.toLowerCase() === address.toLowerCase()) {
-                        outflow_count++;
-                    }
-                    if (transfer.to.toLowerCase() === address.toLowerCase()) {
-                        inflow_count++;
-                    }
-                });
-
-                // Add token data to the JSON.
-                addressData.traded_tokens[contract] = {
-                    symbol: stats.symbol,
-                    spent: Number(stats.spent.toFixed(4)),
-                    pnl: {
-                        total: Number(realizedPnl.toFixed(4)),
-                        realized: Number(stats.received.toFixed(4)),
-                        unrealized: Number(stats.balance.toFixed(4)),
+                // Add calculated data to JSON.
+                const addressData = {
+                    target_chain: 'bsc', // Add target chain from .env file to the JSON.
+                    active_chains: chains, // Add active chains to the JSON
+                    address_info: {
+                        total_transactions: transactions.length,
+                        total_tokens_traded: Object.entries(tokenData).length,
                     },
-                    transfers: {
-                        inflow_count,
-                        outflow_count,
-                    },
+                    traded_tokens: {},
                 };
+
+                for (const [contract, stats] of Object.entries(tokenData)) {
+                    // Calculate PnL for the token: balance + (received - spent).
+                    const realizedPnl = stats.balance + (stats.received - stats.spent);
+
+                    // Count inflow and outflow transactions.
+                    let inflow_count = 0;
+                    let outflow_count = 0;
+
+                    const tokenTransfers = transfers.filter(i => i.contract === contract);
+
+                    tokenTransfers.forEach(transfer => {
+                        if (transfer.from.toLowerCase() === address.toLowerCase()) {
+                            outflow_count++;
+                        }
+                        if (transfer.to.toLowerCase() === address.toLowerCase()) {
+                            inflow_count++;
+                        }
+                    });
+
+                    // Add token data to the JSON.
+                    addressData.traded_tokens[contract] = {
+                        symbol: stats.symbol,
+                        spent: Number(stats.spent.toFixed(4)),
+                        pnl: {
+                            total: Number(realizedPnl.toFixed(4)),
+                            realized: Number(stats.received.toFixed(4)),
+                            unrealized: Number(stats.balance.toFixed(4)),
+                        },
+                        transfers: {
+                            inflow_count,
+                            outflow_count,
+                        },
+                    };
+                }
+
+                const filePath = `${address}.json`;
+                fs.writeFileSync(filePath, JSON.stringify({[address]: addressData}, null, 2));
+
+                const options = {
+                    caption: `\`${address}\``,
+                    parse_mode: 'Markdown',
+                };
+
+                await bot.sendDocument(chatId, filePath, options);
+                fs.unlinkSync(filePath);
+
+                const menuOptions = {
+                    reply_markup: JSON.stringify({
+                        inline_keyboard: [
+                            [{text: 'Wallet address', callback_data: 'option1'}],
+                        ]
+                    })
+                };
+                await bot.sendMessage(chatId, 'Choose an option:', menuOptions);
+            } else {
+                bot.sendMessage(chatId, `Transactions count address more then ${process.env.TRANSACTIONS_COUNT} \n\`${address}\``, {
+                    parse_mode: 'MarkdownV2',
+                });
+                const menuOptions = {
+                    reply_markup: JSON.stringify({
+                        inline_keyboard: [
+                            [{text: 'Wallet address', callback_data: 'option1'}],
+                        ]
+                    })
+                };
+                await bot.sendMessage(chatId, 'Choose an option:', menuOptions);
             }
-
-            const filePath = `${address}.json`;
-            fs.writeFileSync(filePath, JSON.stringify({[address]: addressData}, null, 2));
-
-            const options = {
-                caption: `\`${address}\``,
-                parse_mode: 'Markdown',
-            };
-
-            await bot.sendDocument(chatId, filePath, options);
-            fs.unlinkSync(filePath);
-
-            const menuOptions = {
-                reply_markup: JSON.stringify({
-                    inline_keyboard: [
-                        [{text: 'Wallet address', callback_data: 'option1'}],
-                    ]
-                })
-            };
-            await bot.sendMessage(chatId, 'Choose an option:', menuOptions);
-
         } catch (error) {
             console.error(`Error parsing wallet ${address}:`, error.message);
             await bot.sendMessage(chatId, `Error parsing wallet \`${address}\`: ${error.message}`, {parse_mode: 'Markdown'});
         }
+
     }
 };
 
